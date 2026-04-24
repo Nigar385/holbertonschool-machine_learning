8ff5d5fbe4f7b30a376a1f91f32ede4b85bc8f41
Nigar385 <13627@holbertonstudents.com>
2026-04-24 18:52:56 +0000

    nigar

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that defines np_elementwise function"""
+
+
+def np_elementwise(mat1, mat2):
+    """Performs element-wise operations on numpy arrays"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
