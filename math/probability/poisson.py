d62ff352f6bde41b70bac194a2e13450d29acab7
Nigar385 <13627@holbertonstudents.com>
2026-05-09 14:10:03 +0400

    Update poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
-"""Poisson distribution module"""
+"""
+Contains the Poisson class to represent a poisson distribution
+"""
 
 
 class Poisson:
-    """Represents a Poisson distribution"""
-
-    e = 2.7182818285
+    """
+    Class that represents a poisson distribution
+    """
 
     def __init__(self, data=None, lambtha=1.):
-        """Initialize Poisson distribution"""
+        """
+        Initialize Poisson distribution
+        """
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -17,24 +21,46 @@ class Poisson:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))
-
-    def factorial(self, n):
-        """Calculates factorial of n"""
-        fact = 1
-        for i in range(1, n + 1):
-            fact *= i
-        return fact
+                raise ValueError("data must contain at least multiple values")
+            self.lambtha = sum(data) / len(data)
 
     def pmf(self, k):
-        """Calculates PMF for a given k"""
-        if not isinstance(k, int):
-            k = int(k)
-
+        """
+        Calculates the value of the PMF for a given number of 'successes'
+        """
+        k = int(k)
         if k < 0:
             return 0
 
-        return ((self.e ** (-self.lambtha)) *
-                (self.lambtha ** k) /
-                self.factorial(k))
+        e = 2.7182818285
+        
+        # Factorial of k
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+
+        # PMF = (e^-lambda * lambda^k) / k!
+        return (e ** (-self.lambtha) * (self.lambtha ** k)) / factorial
+
+    def cdf(self, k):
+        """
+        Calculates the value of the CDF for a given number of 'successes'
+        """
+        k = int(k)
+        if k < 0:
+            return 0
+
+        e = 2.7182818285
+        cdf_val = 0
+        
+        # Sum of PMFs from 0 to k
+        for i in range(k + 1):
+            # Calculate factorial for each i
+            factorial = 1
+            for j in range(1, i + 1):
+                factorial *= j
+            
+            # Add PMF of i to total
+            cdf_val += (e ** (-self.lambtha) * (self.lambtha ** i)) / factorial
+            
+        return cdf_val
